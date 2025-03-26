25861995423b039fdd14109433cb59c8f7323674
Gus303 <gurocha05@gmail.com>
2025-03-26 17:56:38 -0300

    aaa

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,11 +1,18 @@
-'use client'
+import { Button } from "@/components/ui/button";
+import {FiDownload} from "react-icons/fi"
 
 export default function Home() {
   return (
- <section>
+ <section className="h-full">
   <div className="container mx-auto h-full">
-    <div className="">
-      <div>text</div>
+    <div className="flex flex-col md:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+      <div className="text-center md:text-left">
+        <span>Web Developer</span>
+        <h1>Hello,
+        <br />  
+        <span>Be Welcome</span></h1>
+        
+      </div>
       <div>photo</div>
     </div>
   </div>
