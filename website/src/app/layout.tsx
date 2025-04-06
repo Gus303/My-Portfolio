fc11fcd121f5683df8c6c3446cd24f50c377a4cf
Gus303 <gurocha05@gmail.com>
2025-04-06 14:36:35 -0300

    testing

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -1,16 +1,15 @@
 import type { Metadata } from "next";
-import { JetBrains_Mono } from "next/font/google";
+import { JetBrains_Mono} from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
 import StairTransition from "@/components/StairTransition";
 
-const jetbrainsMono = JetBrains_Mono({
+ const jetbrainsMono = JetBrains_Mono({
   subsets: ["latin"],
-  weight: ["100","200","300","400","500","600","700","800"],
-  variable: "--font-jetbrainsMono",
-});
-
+   weight: ["100","200","300","400","500","600","700","800"],
+   variable: "--font-jetbrainsMono",
+ });
 
 export const metadata: Metadata = {
   title: "Gustavo's Portfolio",
--- website/src/components/Header.tsx
+++ website/src/components/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
     <header className="py-8 xl:py-12 text-white ">
         <div className="container p-2 mx-auto flex justify-between items-center">
             <Link href='/'>
-            <h1 className="text-4xl font-semibold">Gus<span className="text-sky-400">3</span>0<span className="text-sky-400">3</span></h1>
+            <h3 className="text-4xl font-semibold">Gus<span className="text-sky-400">3</span>0<span className="text-sky-400">3</span></h3>
             </Link>
             <div className="hidden md:flex items-center gap-8 ">
             <Navbar />
